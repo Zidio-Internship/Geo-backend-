17bec9452bac83d9b0b1326812047fdcb0e2aba4
urizennnn <igamerryt@gmail.com>
2024-02-23 14:03:13 +0100

    fixed fb

--- Auth/fb.ts
+++ Auth/fb.ts
@@ -5,7 +5,7 @@ export const facebookAuth = () => {
       clientID: process.env.FACEBOOK_APP_ID as string,
       clientSecret: process.env.FACEBOOK_APP_SECRET as string,
       callbackURL:
-        "http://localhost:8080/login/auth/facebook/redirect" || process.env.FB_URL,
+        "http://localhost:8080/login/auth/facebook/redirect" || process.env.FB_URL as string,
       profileFields: ["id", "displayName", "photos", "email"],
       enableProof: true,
     },
